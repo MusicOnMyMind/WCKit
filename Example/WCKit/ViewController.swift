2a4308bf764cd75e34d613250f67f22a1970f8cd
Mr.He <hewc@MrHedeMacBook-Pro.local>
2019-10-23 14:26:30 +0800

    Test

--- Example/WCKit/ViewController.swift
+++ Example/WCKit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var width = wc_screenWidth - 16 * 2
     }
 
     override func didReceiveMemoryWarning() {
