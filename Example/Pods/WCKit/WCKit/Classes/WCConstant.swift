bb4b7b7440fc5caff05ff257519558bd29c56cec
Mr.He <hewc@MrHedeMacBook-Pro.local>
2019-10-23 14:13:13 +0800

    添加修饰词,避免无法引用的问题

---
+++ Example/Pods/WCKit/WCKit/Classes/WCConstant.swift
@@ -0,0 +1,15 @@
+//
+//  WCConstant.swift
+//  WCKit
+//
+//  Created by Mr.He on 2019/10/23.
+//
+
+import UIKit
+
+public let wc_screenWidth = UIScreen.main.bounds.width
+public let wc_screenHeight = UIScreen.main.bounds.height
+
+public let wc_application = UIApplication.shared
+public let wc_userDefaults = UserDefaults.standard
+public let wc_notificationCenter = NotificationCenter.default
